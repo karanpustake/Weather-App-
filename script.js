db7ff9e9d087b41cc0270b1cf6183780471beb16
Karan Pustake <deangtech@gmail.com>
2023-09-06 14:04:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// api
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd5268f67c4mshaca0375bc5f0fc9p19069djsn5ce4e7c73bcb',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+
+
+const getWeather = (city) =>{
+
+    cityName.innerHTML = city
+
+
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+    .then(response => response.json())
+    .then((response) => {
+
+        console.log(response);
+        temp.innerHTML= response.temp
+        temp2.innerHTML= response.temp
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        humidity2.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        wind_speed.innerHTML = response.wind_speed
+        wind_degrees.innerHTML = response.wind_degrees
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+
+    })
+    .catch(err => console.error(err));}
+
+
+    submit.addEventListener("click", (e)=>
+    {
+        e.preventDefault()
+        getWeather(city.value)
+    })
+
+    getWeather("Delhi")
